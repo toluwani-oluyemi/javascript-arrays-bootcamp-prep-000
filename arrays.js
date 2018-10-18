e938a0d33b21e38f84555159b2c5ccfe1497407a
Toluwani Oluyemi <toluwani.oluyemi@gmail.com>
2018-10-18 23:53:57 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var chocolateBars = [
+  "snickers", "hundred grand", "kitkat", "skittles"
+  ]
+  
+ var addElementToBeginningofArray(array,element){
+    addElementToBeginningofArray.unshift("Book")
+  }
+  destructivelyAddElementToBeginningOfArray
