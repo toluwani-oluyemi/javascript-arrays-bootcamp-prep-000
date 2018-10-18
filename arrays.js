83deef5be8e105b3ce0329fb9dce03e0f4345301
Toluwani Oluyemi <toluwani.oluyemi@gmail.com>
2018-10-18 23:58:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,8 @@ var chocolateBars = [
   "snickers", "hundred grand", "kitkat", "skittles"
   ]
   
- function addElementToBeginningofArray(array,element){
+ function addElementToBeginningofArray(array,element)
+ {
   return [element,...array]
  }
   
