bf97cca9b41c3b1781c675fddce8c70b772697e6
Toluwani Oluyemi <toluwani.oluyemi@gmail.com>
2018-10-19 01:15:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,7 +42,7 @@ function destructivelyRemoveElementFromEndOfArray(array)
 }
   function removeElementFromEndOfArray(array)
   {
-    array.slice(0, array.length-1);
+    array.slice(0, array.length - 1);
     return [...array]
   }
   
