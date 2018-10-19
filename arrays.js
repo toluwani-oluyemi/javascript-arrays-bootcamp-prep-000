6161e8814b697e880fb4ed6b9ee82bdcc0ec0c9b
Toluwani Oluyemi <toluwani.oluyemi@gmail.com>
2018-10-19 00:56:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,6 +7,28 @@ var chocolateBars = [
 [element,...array]
 return array
  }
+  function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array,element)
+ {
+   array.push(element)
+  return [array,element]
+ }
   
+  function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+  }
+  function accessElementInArray(array,index) {
+    return[element,index]
+  }
+  
+  function destructivelyRemoveElementToEndOfArray(array){
+  array.unshift
+  return array
+  } 
+
  
   
