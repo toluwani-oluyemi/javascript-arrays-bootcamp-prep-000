b1243bfbd7d2dca94f2ad69ec438342bd37d6313
Toluwani Oluyemi <toluwani.oluyemi@gmail.com>
2018-10-19 00:42:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ var chocolateBars = [
  function addElementToBeginningofArray(array,element)
  {
   return  [... array,element]
-  var addElementToBeginningofArray = ... array
+
  }
   
   function destructivelyAddElementToBeginningOfArray(array,element){
